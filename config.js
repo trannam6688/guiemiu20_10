96b723c46008ae067b02b26a393fcb1c97610fb9
trannam6688 <116262548+trannam6688@users.noreply.github.com>
2022-10-20 16:01:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Nguyễn Thanh Thảo',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnYes: 'iu nhắm <33',
+    btnNo: 'Hong iu dou:3',
+    question: 'Sao em lại iu anh thía <3',
+    btnReply: 'Gửi cho Anh <3',
+    reply: 'Vì anh đẹp try vllll <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Chúc em yêu của anh có 1 ngày 20/10 thật vui vẻ. Chúc em ngày càng xinh đẹp, học giỏi và thành công với ước mơ của mình. Mãi yêu nhau em nhé.',
+    btnAccept: 'Okiiiii anh iu <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
